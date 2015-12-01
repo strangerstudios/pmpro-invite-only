f1bb23dbcea0c96ef85453f754967b00b80dadd2
Thomas Sjolshagen <eighty20results@users.noreply.github.com>
2015-12-01 12:31:11 -0500

    Updated version number
    Fix: WP won't update to the non-typo'd email body hook.

--- pmpro-invite-only.php
+++ pmpro-invite-only.php
@@ -3,7 +3,7 @@
 Plugin Name: Paid Memberships Pro - Invite Only Add On
 Plugin URI: http://www.paidmembershipspro.com/add-ons/pmpro-invite-only/
 Description: Users must have an invite code to sign up for certain levels. Users are given an invite code to share.
-Version: .3
+Version: .3.1
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
